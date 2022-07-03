960bcb140fe0896d7ea14eb600e26bfa894c6c4f
jorgeherrera1004 <76541335+jorgeherrera1004@users.noreply.github.com>
2022-07-03 15:00:23 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Navegation from "./Navegation";
+
+export default function App() {
+	return <Navegation />;
+}
---
+++ Navegation.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+//Icons
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+
+//screens
+import HomeScreen from "./screens/HomeScreen";
+import AboutScreen from "./screens/AboutScreen";
+
+//screens Stack Movie
+import Movie_principal from "./screens/Stack_Movie/Principal";
+import Movie_View from "./screens/Stack_Movie/View";
+
+//Home Stack
+
+const HomeStackNavigator = createNativeStackNavigator();
+const HomeStack = () => {
+	return (
+		<HomeStackNavigator.Navigator initialRouteName="HomeScreen">
+			<HomeStackNavigator.Screen name="HomeScreen" component={HomeScreen} />
+			<HomeStackNavigator.Screen name="Stack" component={AboutScreen} />
+		</HomeStackNavigator.Navigator>
+	);
+};
+
+//Movies Stack
+
+const MovieStackNavigator = createNativeStackNavigator();
+const MovieStack = () => {
+	return (
+		<MovieStackNavigator.Navigator initialRouteName="Movie_principal">
+			<MovieStackNavigator.Screen
+				name="Movie_principal"
+				component={Movie_principal}
+			/>
+			<MovieStackNavigator.Screen name="Movie_View" component={Movie_View} />
+		</MovieStackNavigator.Navigator>
+	);
+};
+
+//Tab
+const Tab = createBottomTabNavigator();
+
+const MyTabs = () => {
+	return (
+		<Tab.Navigator
+			initialRouteName="Home"
+			screenOptions={{ tabBarActiveTintColor: "purple" }}>
+			<Tab.Screen
+				name="Home"
+				component={HomeStack}
+				options={{
+					tabBarLabel: "Home",
+					tabBarIcon: ({ color, size }) => (
+						<MaterialCommunityIcons name="home" size={size} color={color} />
+					),
+				}}
+			/>
+			<Tab.Screen
+				name="Movie"
+				component={MovieStack}
+				options={{
+					tabBarLabel: "MovieScreen",
+					tabBarIcon: ({ color, size }) => (
+						<MaterialCommunityIcons name="movie" size={size} color={color} />
+					),
+				}}
+			/>
+			<Tab.Screen
+				name="Creditos"
+				component={AboutScreen}
+				options={{
+					tabBarLabel: "Creditos",
+					tabBarIcon: ({ color, size }) => (
+						<Entypo name="info-with-circle" size={size} color={color} />
+					),
+				}}
+			/>
+		</Tab.Navigator>
+	);
+};
+
+const Navegation = () => {
+	return (
+		<NavigationContainer>
+			<MyTabs />
+		</NavigationContainer>
+	);
+};
+
+export default Navegation;
